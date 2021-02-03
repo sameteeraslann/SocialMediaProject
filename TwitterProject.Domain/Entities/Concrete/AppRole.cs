7476247640499304cfc375ec9d003867ab10cf33
Samet <samettt18@hotmail.com.tr>
2021-02-03 18:09:45 +0300

    AppRole Eklendi

---
+++ TwitterProject.Domain/Entities/Concrete/AppRole.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwitterProject.Domain.Entities.Concrete
+{
+    public class AppRole: BaseEntity<int>
+    {
+
+    }
+}
