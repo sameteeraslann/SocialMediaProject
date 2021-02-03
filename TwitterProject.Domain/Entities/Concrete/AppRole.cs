d7a809970ecbcc58120f1f1dd0bd319bab69ed8b
Samet <samettt18@hotmail.com.tr>
2021-02-03 18:26:48 +0300

    AppRole.cs açılır

--- TwitterProject.Domain/Entities/Concrete/AppRole.cs
+++ TwitterProject.Domain/Entities/Concrete/AppRole.cs
@@ -1,11 +1,21 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using TwitterProject.Domain.Entities.Interface;
+using TwitterProject.Domain.Enums;
 
 namespace TwitterProject.Domain.Entities.Concrete
 {
-    public class AppRole: BaseEntity<int>
+    public class AppRole: IdentityRole<int>,IBaseEntity
     {
+        private DateTime _createDate = DateTime.Now;
+        public DateTime CreateDate { get => _createDate; set => value = _createDate; }
 
+        public DateTime? ModifiedDate { get; set; }
+        public DateTime? DeleteDate { get; set; }
+
+        private Status _status = Status.Active;
+        public Status Status { get => _status; set => value = _status; }
     }
 }
