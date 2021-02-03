44279e1371ae4a86da9eb88a05a561c3af6f1dee
Samet <samettt18@hotmail.com.tr>
2021-02-03 18:08:54 +0300

    BaseEntity Eklendi

---
+++ TwitterProject.Domain/Entities/Concrete/BaseEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TwitterProject.Domain.Entities.Interface;
+using TwitterProject.Domain.Enums;
+
+namespace TwitterProject.Domain.Entities.Concrete
+{
+    public abstract class BaseEntity<T> : IBaseEntity
+    {
+        public T Id { get; set; }
+
+        private DateTime _createDate = DateTime.Now;
+        public DateTime CreateDate { get => _createDate; set => value = _createDate; }
+
+        public DateTime? ModifiedDate { get; set; }
+        public DateTime? DeleteDate { get; set; }
+
+        private Status _status = Status.Active;
+        public Status Status { get => _status; set => value = _status; }
+    }
+}
