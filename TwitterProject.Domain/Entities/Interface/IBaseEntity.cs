152cd45f37cb0a470430bab07995e7532c3e235e
Samet <samettt18@hotmail.com.tr>
2021-02-03 18:07:26 +0300

    IBaseEntity Eklendi

---
+++ TwitterProject.Domain/Entities/Interface/IBaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TwitterProject.Domain.Enums;
+
+namespace TwitterProject.Domain.Entities.Interface
+{
+    public interface IBaseEntity
+    {
+        DateTime CreateDate { get; set; }
+        DateTime? ModifiedDate { get; set; }
+        DateTime? DeleteDate { get; set; }
+        Status Status { get; set; }
+    }
+}
