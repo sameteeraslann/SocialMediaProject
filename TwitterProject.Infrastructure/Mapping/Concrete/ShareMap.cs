a6d2c1cdbaf37f79ececf15b9c965c321b3d5a4f
Samet <samettt18@hotmail.com.tr>
2021-02-04 16:44:53 +0300

    ShareMap Eklendi

---
+++ TwitterProject.Infrastructure/Mapping/Concrete/ShareMap.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TwitterProject.Domain.Entities.Concrete;
+using TwitterProject.Infrastructure.Mapping.Abstract;
+
+namespace TwitterProject.Infrastructure.Mapping.Concrete
+{
+    public class ShareMap : BaseMap<Share>
+    {
+        public override void Configure(EntityTypeBuilder<Share> builder)
+        {
+            builder.HasKey(x => new { x.AppUserId, x.TweetId });
+
+            base.Configure(builder);
+        }
+    }
+}
