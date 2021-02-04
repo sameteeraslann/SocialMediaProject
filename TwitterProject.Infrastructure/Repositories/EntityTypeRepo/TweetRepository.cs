270e063d2a425e61d0e4abcfc62402afd8e081de
Samet <samettt18@hotmail.com.tr>
2021-02-04 19:23:38 +0300

    TweetRepository oluşturuldu

---
+++ TwitterProject.Infrastructure/Repositories/EntityTypeRepo/TweetRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TwitterProject.Domain.Entities.Concrete;
+using TwitterProject.Domain.Repositories.EntityTypeRepo;
+using TwitterProject.Infrastructure.Context;
+using TwitterProject.Infrastructure.Repositories.BaseRepo;
+
+namespace TwitterProject.Infrastructure.Repositories.EntityTypeRepo
+{
+    public class TweetRepository : BaseRepository<Tweet>, ITweetRepository
+    {
+        public TweetRepository(ApplicationDbContext applicationDbContext) : base(applicationDbContext) { }
+    }
+}
