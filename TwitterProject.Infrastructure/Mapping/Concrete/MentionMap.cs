aefba64b6b595011d1d5c8c2fa76b653a27ef0c7
Samet <samettt18@hotmail.com.tr>
2021-02-04 16:43:24 +0300

    MentionMap Eklendi

---
+++ TwitterProject.Infrastructure/Mapping/Concrete/MentionMap.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TwitterProject.Domain.Entities.Concrete;
+using TwitterProject.Infrastructure.Mapping.Abstract;
+
+namespace TwitterProject.Infrastructure.Mapping.Concrete
+{
+    public class MentionMap : BaseMap<Mention>
+    {
+        public override void Configure(EntityTypeBuilder<Mention> builder)
+        {
+            builder.HasKey(x => new { x.Id, x.AppUserId, x.TweetId });
+            builder.Property(x => x.Text).IsRequired(false);
+            base.Configure(builder);
+        }
+    }
+}
