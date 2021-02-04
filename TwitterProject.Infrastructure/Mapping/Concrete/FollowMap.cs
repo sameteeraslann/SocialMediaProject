f7415e2c976cf006f2651e4cf0e93df87f361d8c
Samet <samettt18@hotmail.com.tr>
2021-02-04 16:41:55 +0300

    FollowMap Eklendi

---
+++ TwitterProject.Infrastructure/Mapping/Concrete/FollowMap.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TwitterProject.Domain.Entities.Concrete;
+using TwitterProject.Infrastructure.Mapping.Abstract;
+
+namespace TwitterProject.Infrastructure.Mapping.Concrete
+{
+    public class FollowMap : BaseMap<Follow>
+    {
+        public override void Configure(EntityTypeBuilder<Follow> builder)
+        {
+            builder.HasKey(x => new { x.FollowerId, x.FollowingId });
+
+            base.Configure(builder);
+        }
+
+    }
+}
