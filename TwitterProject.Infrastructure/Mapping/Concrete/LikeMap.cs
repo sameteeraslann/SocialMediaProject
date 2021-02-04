e296385cf25a0782d42603d220cbdc684731b80d
Samet <samettt18@hotmail.com.tr>
2021-02-04 16:42:37 +0300

    LikeMap Eklendi.

---
+++ TwitterProject.Infrastructure/Mapping/Concrete/LikeMap.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TwitterProject.Domain.Entities.Concrete;
+using TwitterProject.Infrastructure.Mapping.Abstract;
+
+namespace TwitterProject.Infrastructure.Mapping.Concrete
+{
+    public class LikeMap : BaseMap<Like>
+    {
+        public override void Configure(EntityTypeBuilder<Like> builder)
+        {
+            builder.HasKey(x => new { x.AppUserId, x.TweetId });
+            base.Configure(builder);
+        }
+    }
+}
