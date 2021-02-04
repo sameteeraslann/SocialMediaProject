44c67abed5a9bc5e482ee4ac3aa0aa3eb0123366
Samet <samettt18@hotmail.com.tr>
2021-02-04 19:18:23 +0300

    LikeRepository oluşturuldu

---
+++ TwitterProject.Infrastructure/Repositories/EntityTypeRepo/LikeRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TwitterProject.Domain.Entities.Concrete;
+using TwitterProject.Domain.Repositories.EntityTypeRepo;
+using TwitterProject.Infrastructure.Context;
+using TwitterProject.Infrastructure.Repositories.BaseRepo;
+
+namespace TwitterProject.Infrastructure.Repositories.EntityTypeRepo
+{
+    public class LikeRepository : BaseRepository<Like>, ILikeRepository
+    {
+        public LikeRepository(ApplicationDbContext applicationDbContext) : base(applicationDbContext) { }
+    }
+}
