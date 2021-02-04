600dd2f51901d8ff09c926f3fa9fda9f8deb91f3
Samet <samettt18@hotmail.com.tr>
2021-02-04 19:21:58 +0300

    AppUserRepository oluşturuldu

--- TwitterProject.Infrastructure/Repositories/EntityTypeRepo/AppUserRepository.cs
+++ TwitterProject.Infrastructure/Repositories/EntityTypeRepo/AppUserRepository.cs
@@ -8,8 +8,8 @@ using TwitterProject.Infrastructure.Repositories.BaseRepo;
 
 namespace TwitterProject.Infrastructure.Repositories.EntityTypeRepo
 {
-    public class AppUserRepository : BaseRepository<AppUser>, IAppUserRepository
+    public class AppUserRepository : BaseRepository<AppUser>, IAppUserRepository // => BaseRepository<AppUser> tipinde kalıtım aldık. Daha sonra inject edeceğimiz IAppUserRepository tanımladık. Bunu yapmamızın amacı DIP prensibine uymamız. Sınıfları olabildiğince birbirinden bağımsız hale getirmek.
     {
-        public AppUserRepository(ApplicationDbContext applicationDbContext) : base(applicationDbContext) { }
+        public AppUserRepository(ApplicationDbContext applicationDbContext) : base(applicationDbContext) { } //=> Database bağlantısını yaptık.
     }
 }
