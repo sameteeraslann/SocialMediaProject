36f1e3a04316e8524cadb868f6d9aeb462489a51
Samet <samettt18@hotmail.com.tr>
2021-02-04 16:41:13 +0300

    AppUserMap.ca Eklendi

---
+++ TwitterProject.Infrastructure/Mapping/Concrete/AppUserMap.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TwitterProject.Domain.Entities.Concrete;
+using TwitterProject.Infrastructure.Mapping.Abstract;
+
+namespace TwitterProject.Infrastructure.Mapping.Concrete
+{
+    public class AppUserMap : BaseMap<AppUser>
+    {
+        public override void Configure(EntityTypeBuilder<AppUser> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.UserName).IsRequired(false);
+            builder.Property(x => x.NormalizedUserName).HasMaxLength(50);
+            builder.Property(x => x.Name).HasMaxLength(50).IsRequired(true);
+            builder.Property(x => x.ImagePath).HasMaxLength(50).IsRequired(true);
+
+
+
+            builder.HasMany(x => x.Tweets).WithOne(x => x.AppUser).HasForeignKey(x => x.AppUserId);
+            builder.HasMany(x => x.Mentions).WithOne(x => x.AppUser).HasForeignKey(x => x.AppUserId);
+            builder.HasMany(x => x.Likes).WithOne(x => x.AppUser).HasForeignKey(x => x.AppUserId);
+            builder.HasMany(x => x.Shares).WithOne(x => x.AppUser).HasForeignKey(x => x.AppUserId);
+
+            builder.HasMany(x => x.Followers).WithOne(x => x.Follower).HasForeignKey(x => x.FollowerId);
+            builder.HasMany(x => x.Followings).WithOne(x => x.Following).HasForeignKey(x => x.FollowingId);
+
+
+
+            base.Configure(builder);
+
+        }
+    }
+}
